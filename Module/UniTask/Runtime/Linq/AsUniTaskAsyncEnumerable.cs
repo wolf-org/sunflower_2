a36a94c37bcd545dae5de2e48991ea391ca7cc90
Wolf <ducnv.cntt0502@gmail.com>
2024-05-02 22:05:42 +0700

    -add: UniTask

---
+++ Module/UniTask/Runtime/Linq/AsUniTaskAsyncEnumerable.cs
@@ -0,0 +1,10 @@
+﻿namespace VirtueSky.Threading.Tasks.Linq
+{
+    public static partial class UniTaskAsyncEnumerable
+    {
+        public static IUniTaskAsyncEnumerable<TSource> AsUniTaskAsyncEnumerable<TSource>(this IUniTaskAsyncEnumerable<TSource> source)
+        {
+            return source;
+        }
+    }
+}
