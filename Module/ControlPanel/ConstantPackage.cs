232fa904c4b1f513960b4fce21507350dc433b1f
DucNV <ducnv@gameestudio.com>
2024-07-05 10:28:21 +0700

    - update version

--- Module/ControlPanel/ConstantPackage.cs
+++ Module/ControlPanel/ConstantPackage.cs
@@ -2,7 +2,7 @@
 {
     public class ConstantPackage
     {
-        public const string VersionUnityCommon = "1.1.4";
+        public const string VersionUnityCommon = "1.1.5";
         public const string PackageNameInAppPurchase = "com.unity.purchasing";
         public const string MaxVersionInAppPurchase = "4.11.0";
         public const string PackageNameNewtonsoftJson = "com.unity.nuget.newtonsoft-json";
