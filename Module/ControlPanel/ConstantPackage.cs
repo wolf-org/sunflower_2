e67119fdf8dc3d4bcbad26d1d6555b92e3af982f
Wolf <ducnv.cntt0502@gmail.com>
2024-07-18 00:26:38 +0700

    - update version

--- Module/ControlPanel/ConstantPackage.cs
+++ Module/ControlPanel/ConstantPackage.cs
@@ -2,7 +2,7 @@
 {
     public class ConstantPackage
     {
-        public const string VersionUnityCommon = "1.1.7";
+        public const string VersionUnityCommon = "1.1.8";
         public const string PackageNameInAppPurchase = "com.unity.purchasing";
         public const string MaxVersionInAppPurchase = "4.11.0";
         public const string PackageNameNewtonsoftJson = "com.unity.nuget.newtonsoft-json";
