26fd3f226f63b6c0d622c7b19cda4b6119c803ca
DucNV <ducnv@gameestudio.com>
2024-11-12 13:36:58 +0700

    - update version
    Co-Authored-By: VirtueSky <126542083+VirtueSky@users.noreply.github.com>

--- Module/ControlPanel/ConstantPackage.cs
+++ Module/ControlPanel/ConstantPackage.cs
@@ -2,7 +2,7 @@
 {
     public class ConstantPackage
     {
-        public const string VersionUnityCommon = "1.4.3";
+        public const string VersionUnityCommon = "1.4.4";
         public const string PackageNameInAppPurchase = "com.unity.purchasing";
         public const string MaxVersionInAppPurchase = "4.12.2";
         public const string PackageNameNewtonsoftJson = "com.unity.nuget.newtonsoft-json";
--- README.md
+++ README.md
@@ -23,9 +23,9 @@
 ### 1: Download the repo and drop it into folder `Assets`
 ### 2: Add the line below to `Packages/manifest.json`
 
-for version `1.4.3`
+for version `1.4.4`
 ```csharp
-"com.wolf-org.sunflower2":"https://github.com/wolf-org/sunflower_2.git#1.4.3",
+"com.wolf-org.sunflower2":"https://github.com/wolf-org/sunflower_2.git#1.4.4",
 ```
 
 ## Includes modules
