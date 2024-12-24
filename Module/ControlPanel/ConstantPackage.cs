a8aae90307754a23e3ec46fbce68a69a164f8e7e
Ducnv <virtuesky.0520@gmail.com>
2024-12-24 21:26:34 +0700

    - update version

--- Module/ControlPanel/ConstantPackage.cs
+++ Module/ControlPanel/ConstantPackage.cs
@@ -2,7 +2,7 @@
 {
     public class ConstantPackage
     {
-        public const string VersionUnityCommon = "1.4.8";
+        public const string VersionUnityCommon = "1.4.9";
         public const string PackageNameInAppPurchase = "com.unity.purchasing";
         public const string MaxVersionInAppPurchase = "4.12.2";
         public const string PackageNameNewtonsoftJson = "com.unity.nuget.newtonsoft-json";
